18302280e445cc375f6d116418284047a5aacf6f
A01661736 <88848934+A01661736@users.noreply.github.com>
2021-09-06 12:22:17 -0500

    Update exercise.py

--- assignments/17NCuadradoMayor/src/exercise.py
+++ assignments/17NCuadradoMayor/src/exercise.py
@@ -2,6 +2,18 @@
 
 def main():
     #Escribe tu código debajo de esta línea
+    print ("Escribe un número: ", end="")
+    n=int(input())
+    i=1
+    b=0
+    while b<=n:
+        
+        i=i+1
+        b=i**2
+        continue
+
+    print (str(i))
+    
     pass
 
 if __name__=='__main__':
