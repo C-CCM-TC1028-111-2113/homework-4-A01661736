f6d4bb7160bf6e3e22d8bce332c76193401ac145
A01661736 <88848934+A01661736@users.noreply.github.com>
2021-09-06 11:32:37 -0500

    Update exercise.py

--- assignments/06PromedioConDecision/src/exercise.py
+++ assignments/06PromedioConDecision/src/exercise.py
@@ -1,5 +1,23 @@
 def main():
     #escribe tu código abajo de esta línea
+    print ("Type the numbers you'd want to sum and get the average of. The program will end once you input a negative number.")
+    a=0
+    suma=0
+    nums=0
+    while a>=0:
+        oanw
+        a=float(input())
+        if (a<0):
+            break
+    
+        suma=suma+a
+        nums=nums+1
+
+
+    promedio=suma/nums
+
+    print("The average of the numbers is: " + str(promedio))
+
     pass
 if __name__=='__main__':
     main()
